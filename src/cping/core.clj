b1e2d5c6f5ba1e77185279dad74b990fd274f8cc
Christoph Neumann <christoph@neumannhaus.com>
2014-01-30 11:48:27 -0500

    Have the basic looping and summary information working. Don't have any of the date and timing information yet.

--- src/cping/core.clj
+++ src/cping/core.clj
@@ -6,7 +6,27 @@
 (defn- ping!
   "Use the shell to ping the given host"
   [host]
-  (sh "ping" "-c" "1" host))
+  (sh "ping" "-W" "1" "-c" "1" host))
+
+
+(defn- nstr
+  "Repeat the given string N times"
+  [n string]
+  (apply str (repeat n string)))
+
+
+(defn summarize
+  "Create the summary histogram of"
+  [good]
+  (let [last_10 (max 0 (- good 50))
+        first_50 (int (/ (+ (min 0 (- good 50)) 50) 10))]
+    (format "|%s%s%s%s| %s"
+            (nstr first_50 "#")
+            (nstr (- 5 first_50) ".")
+            (nstr last_10 "%")
+            (nstr (- 10 last_10) ".")
+            good)))
+
 
 (defn -main
   "Main method for the cping app."
@@ -18,9 +38,19 @@
 
   (let [host (first args)]
     (println (str "The host is: " host))
-    (let [{:keys [out exit] :as result} (ping! host)]
-      (println out)
-      (println "Exit code:" exit)
-      (println "Ping" (if (= exit 0) "succeeded" "failed"))))
+    (let [sec (atom 0)
+          good (atom 0)]
+      (while true
+        (let [{:keys [out exit] :as result} (ping! host)]
+          (swap! sec inc)
+          (if (= exit 0) (swap! good inc))
+          (print (if (= exit 0) "#" "."))
+          (flush)
+          (if (= @sec 60)
+            (do
+              (println (summarize @good))
+              (reset! sec 0)
+              (reset! good 0)))
+          (Thread/sleep 500)))))
 
   (System/exit 0))
